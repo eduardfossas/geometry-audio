01d3db4ebf4bda1a91c580686e0897527fab1096
Eduard Fossas <eduard@rocani.studio>
2023-08-02 20:48:46 +0200

    Release!

---
+++ app.js
@@ -0,0 +1,48 @@
+const BACK_COLOR = "#333";
+const STICK_COLOR = "#faf0e6";
+
+class Rectangle {
+  constructor(x, y, w, h, context, color) {
+    context.fillRect(x, y, w, h);
+  }
+}
+
+class App {
+  constructor() {
+    const synth = new Tone.Synth().toDestination();
+    this.svg = document.querySelector("svg");
+
+    console.log(this.svg);
+
+    this.svg.addEventListener("click", (e) => {
+      let className = e.target.className.baseVal;
+      switch (className) {
+        case "square":
+          synth.triggerAttackRelease("C4", "10n");
+          break;
+
+        case "circle":
+          synth.triggerAttackRelease("E4", "10n");
+          break;
+
+        case "triangle":
+          synth.triggerAttackRelease("D4", "10n");
+          break;
+
+        case "hex":
+          synth.triggerAttackRelease("G4", "10n");
+          break;
+        case "star":
+          synth.triggerAttackRelease("Cb4", "10n");
+          break;
+
+        default:
+          break;
+      }
+    });
+  }
+
+  handleMouseEvent() {}
+}
+
+new App();
